d12e4bbd2f4ec22646b1475f4aa5a3f5cbc3ffc8
Jessica Grebenschikov <jessica.greben1@gmail.com>
2015-04-17 19:27:42 -0700

    change favicon, add media queries, add drop down project

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
+	$('#toggle-menu').hide();
+	$('.more-content').hide();
+	$('.lifetime-content').hide();
+	$('.tobitch-content').hide();
 
+
+		$('.more-me').on('click', function(event) {
+			event.preventDefault();
+			$('.more-content').toggle();
+		})
+
+		$('.container').on('click', '.menu-nav', function(event) {
+			event.preventDefault();
+			$('#toggle-menu').toggle();
+		})
+
+		$('.lifetime-link').on('click', function(event) {
+			event.preventDefault();
+			$('.lifetime-content').toggle();
+			$('.tobitch-link').toggle();
+			$('.lifetime-down').toggle();
+			$(this).toggleClass("fa fa-chevron-right 3x");
+		})
+
+		$('.tobitch-link').on('click', function(event) {
+			event.preventDefault();
+			$('.tobitch-content').toggle();
+			$('.lifetime-link').toggle();
+			$('.tobitch-down').toggle();
+			$(this).toggleClass("fa fa-chevron-right 3x");
+
+		})
 });
